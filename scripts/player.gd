52a0731ff1f79bf25590915ef0e34dccc05fa9a2
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-11-16 21:25:09 +1100

    yoooo they're runnin now

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	$Sprite2D.play("default")
 
 func _physics_process(delta):
 	# Add the gravity.
