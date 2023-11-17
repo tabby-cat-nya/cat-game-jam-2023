d9734272eca34dedfa26f87d44b7b9399c87915e
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-11-17 17:48:01 +1100

    basic hud, module with slider

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 @export var gameTime : float = 0
 
+@onready var storm := $killWall
+@onready var hud := $HUD/Label
+
+# old speed 130
+# max speed = player speed (150)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,5 +16,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	gameTime += delta / ( 1 + (gameTime/60))
+	gameTime += delta # / ( 1 + (gameTime/60))
+	storm.Speed = clampf(130+(gameTime/5), 130, 150)
+	hud.text = "Score: " + str(round(gameTime)) + "\nWall Speed: " + str(round(storm.Speed*10)/10)
 	#Engine.time_scale = 1 + (gameTime/60)
