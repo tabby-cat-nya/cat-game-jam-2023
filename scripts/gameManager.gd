5e88d8ce341e38381fb2bb2e722050b93b16543a
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-11-17 21:48:52 +1100

    new tilemap, horizontal slider level

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -5,6 +5,7 @@ var modulesGenerated : int = 0
 
 @onready var storm := $killWall
 @onready var hud := $HUD/Label
+@onready var player := $player
 
 # old speed 130
 # max speed = player speed (150)
@@ -18,8 +19,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	gameTime += delta # / ( 1 + (gameTime/60))
-	storm.Speed = clampf(130+(gameTime/5), 130, 150)
-	hud.text = "Score: " + str(round(gameTime)) + "\nWall Speed: " + str(round(storm.Speed*10)/10) + "\nModules Generated: " + str(modulesGenerated)
+	player.speed = clampf(player.speed + delta/1, 150, 500) 
+	storm.Speed = clampf(130+(gameTime/0.95), 130, player.speed)
+	hud.text = "Score: " + str(round(gameTime)) + "\nWall/Player Speed: " + str(round(storm.Speed*10)/10) + "/" + str(round(player.speed*10)/10) + "\nModules Generated: " + str(modulesGenerated)
 	#Engine.time_scale = 1 + (gameTime/60)
 
 
