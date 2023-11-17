59defb9ef4e464b03f806590f5dc2a02e6aa8552
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-11-17 23:47:23 +1100

    pride flag trails!!! 0.0

---
+++ scripts/Trail.gd
@@ -0,0 +1,23 @@
+extends Line2D
+#class_name Trails
+ 
+var queue : Array
+@export var MAX_LENGTH : int
+@export var targetBody : Node2D
+ 
+func _process(_delta):
+	var pos = _get_position()
+ 
+	queue.push_front(pos)
+ 
+	if queue.size() > MAX_LENGTH:
+		queue.pop_back()
+ 
+	clear_points()
+ 
+ 
+	for point in queue:
+		add_point(point)
+ 
+func _get_position():
+	return targetBody.global_position
