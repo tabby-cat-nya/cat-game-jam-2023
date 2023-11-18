d5d69174c03a32b301caf39fb04a086b0b3dc5c0
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-11-18 18:52:54 +1100

    menu and trials

---
+++ scripts/menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var trailsList = $MarginContainer/VSplitContainer/Panel2/VBoxContainer/ItemList
+@onready var selectedText = $MarginContainer/VSplitContainer/Panel2/VBoxContainer/Label2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_item_list_item_clicked(index, at_position, mouse_button_index):
+	trailsList.get_item_icon(index)
+	selectedText.text = trailsList.get_item_text(index)
+	print("hiiii")
+	pass # Replace with function body.
+
+
+func _on_item_list_item_selected(index):
+	print("heeeeyyyy")
+	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print(event.position)
+		var clickedItem = trailsList.get_item_at_position(event.position - trailsList.global_position)
+		selectedText.text = "Selected Trail: " + trailsList.get_item_text(clickedItem)
